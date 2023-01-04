ee3727729ab9ccbda1963be7c399fc6abb336870
Ricardo Silva <rssilvaba@gmail.com>
2023-01-04 23:56:38 +0100

    added storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+import docJson from "../documentation.json";
+setCompodocJson(docJson);
+
+export const parameters = {
+  layout: 'fullscreen',
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  viewport: {
+    viewports: {
+      small: { name: 'small', styles: { width: '360px', height: '640px' } },
+      medium: { name: 'medium', styles: { width: '540px', height: '720px' } },
+      large: { name: 'large', styles: { width: '720px', height: '90%' } },
+      full: { name: 'full', styles: { width: '100%', height: '100%' } },
+    }
+  },
+  docs: { inlineStories: true },
+}
